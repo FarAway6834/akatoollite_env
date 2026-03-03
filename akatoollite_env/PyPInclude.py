2fa2c86fca9807c6c23ed69e0213bd7567893ba3
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-04 08:40:16 +0900

    PyPInclude.py 업데이트

--- akatoollite_env/PyPInclude.py
+++ akatoollite_env/PyPInclude.py
@@ -6,7 +6,7 @@ from os.path import isfile as __is_file
 from os.path import islink as __is_link
 from os import listdir as ls
 
-def include_dir(project, dir = "include", all_ext_allow = False, root = True, error = False, ret = []):
+def include_dir(project, dir = "include", all_ext_allow = False, error = False, root = True, ret = []):
     target = __join_path(project, dir)
     assert not root or __is_dir(target), FileNotFoundError(f"PyPInclude need directory 'include', but in this project, no directory name {target}")
     if root and not all_ext_allow: all_ext_allow = ("allow" in ls(target))
@@ -14,8 +14,8 @@ def include_dir(project, dir = "include", all_ext_allow = False, root = True, er
     if not all_ext_allow:
         for f in ls(target):
             if __is_dir(f):
-                if error: include_dir(project, dir = __join_path(dir, f))
-                else: include_dir(project, dir = __join_path(dir, f), ret = ret)
+                if error: include_dir(project, dir = __join_path(dir, f), all_ext_allow, error = error, root = False)
+                else: include_dir(project, dir = __join_path(dir, f), all_ext_allow, error = error, root = False, ret = ret)
             elif __is_link(f):
                 if not error: ret.append(__join_path(dir, f))
             elif __is_file(f):
